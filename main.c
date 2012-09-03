27413e36d543cfae4713e82d7fd6363e81b003e1
Brandon Philips <brandon@ifup.org>
2012-09-03 16:32:47 -0700

    main: stream back stdin
    this now streams stdin to the request.

--- main.c
+++ main.c
@@ -3,6 +3,28 @@
 #include <string.h>
 #include <stdio.h>
 
+static ssize_t
+read_callback(void *data, uint64_t pos, char *buf, size_t max)
+{
+  int fd = data;
+  int ret;
+  ret = read(fd, buf, max);
+
+  if (ret == 0)
+    return MHD_CONTENT_READER_END_OF_STREAM;
+  if (ret == -1)
+    return MHD_CONTENT_READER_END_WITH_ERROR;
+
+  return ret;
+}
+
+static void
+free_callback(void *data)
+{
+  int fd = data;
+  close(fd);
+}
+
 static int stdin_echo(void * cls,
 		    struct MHD_Connection * connection,
 		    const char * url,
@@ -30,7 +52,11 @@ static int stdin_echo(void * cls,
     return MHD_NO; /* upload data in a GET!? */
 
   *ptr = NULL; /* clear context pointer */
-  response = MHD_create_response_from_fd(-1, STDIN_FILENO);
+  response = MHD_create_response_from_callback(MHD_SIZE_UNKNOWN,
+               32 * 1024,
+               &read_callback,
+               STDIN_FILENO,
+               &free_callback);
   ret = MHD_queue_response(connection,
 			   MHD_HTTP_OK,
 			   response);
@@ -39,8 +65,7 @@ static int stdin_echo(void * cls,
   return ret;
 }
 
-int main(int argc,
-	 char ** argv) {
+int main(int argc, char ** argv) {
   struct MHD_Daemon * d;
 
   if (argc != 2) {
